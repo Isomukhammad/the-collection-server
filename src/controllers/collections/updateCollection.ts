05cdb1a11785d7cf39d00b95f900130d74c25eae
Isomukhammad <s.isomuxammad@gmail.com>
2024-03-29 01:10:13 +0500

    refactor: remove unused variable

--- src/controllers/collections/updateCollection.ts
+++ src/controllers/collections/updateCollection.ts
@@ -6,7 +6,6 @@ import { decodeJwt } from "../../utils/decodeJwt";
 export const updateCollection = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { name, description, topic, img } = req.body;
-  const user = decodeJwt(req.token as string);
 
   try {
     const updatedCollection = await prisma.collection.update({
